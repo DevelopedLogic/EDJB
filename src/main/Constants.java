374b4d9dc1e50346b288f35785c82944fd651ade
DevelopedLogic <developing11@outlook.com>
2017-09-03 11:32:41 +0100

    Removed accidental token

--- src/main/Constants.java
+++ src/main/Constants.java
@@ -2,7 +2,7 @@ package main;
 
 public interface Constants{
 	//Generalised bot settings
-	public static final String token = "MzUyMTU1MDM4MDM2MTMxODQw.DI1ixg.6MhX5HF7lbFs0G2OKA-w48EIHus"; //Token the bot uses to sign into its bot account
+	public static final String token = ""; //Token the bot uses to sign into its bot account
 	public static final String[] developerIDs = {"312269489850941450"/*DevelopedLogic*/}; //Public IDs of the users who are allowed access to the Developer Only commands
 	public static String prefix = "?!"; //Bot's prefix. Not final so it can be changed by a command during execution
 	public static final String botName = "EDJBot";
